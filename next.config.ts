2542b9f9e36444924099cd7aea55ebfc78134fa7
David Tompkins <etherealsoftwaresolutions@gmail.com>
2025-08-30 12:35:40 -0500

    Build scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: isProd ? '/epk-website' : '',
+  assetPrefix: isProd ? '/epk-website/' : '',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
